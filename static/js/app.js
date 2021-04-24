dbc01ee55058de8c5ef3b1eae65cf6b209bd5335
Hums Shaik <humsshaik@gmail.com>
2021-04-23 22:09:57 -0500

    get data for dropdown

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,18 @@ console.log("app.js loaded");
 
 
 function init() {
-
+     // select dropdown menu 
     var dropdown = d3.select("#selDataset");
 
     // read the data 
     d3.json("data/samples.json").then((data)=> {
         console.log(data)
 
+    // get the id data to the dropdwown menu
+    data.names.forEach(function(name) {
+        dropdown.append("option").text(name).property("value");
+    });
+     // call the functions 
     });
 
 }
