f7dfd5ac3b1e31f5d6347ea7cb74d827c25f83b4
Hums Shaik <humsshaik@gmail.com>
2021-04-23 22:04:26 -0500

    dropdown menu

--- static/js/app.js
+++ static/js/app.js
@@ -4,6 +4,14 @@ console.log("app.js loaded");
 
 function init() {
 
+    var dropdown = d3.select("#selDataset");
+
+    // read the data 
+    d3.json("data/samples.json").then((data)=> {
+        console.log(data)
+
+    });
+
 }
 
 init();
